836141319e2126cfd9fab992ec6798c59db052ee
vinhhh <vinh.hh@holistics.io>
2024-06-23 06:24:26 +0700

    try implement PING

--- app/server.go
+++ app/server.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"net"
 	"os"
 )
 
+func stop_server(l io.Closer) {
+	err := l.Close()
+	if err != nil {
+		fmt.Println("close server error")
+	}
+}
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
@@ -14,9 +22,24 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+
+	c, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
+	}
+
+	defer stop_server(c)
+
+	buf := make([]byte, 128)
+	_, err = c.Read(buf)
+	if err != nil {
+		fmt.Println("Can not read")
+	}
+
+	log.Printf("Read: %s", buf)
+	_, err = c.Write([]byte("+PONG\r\n"))
+	if err != nil {
+		fmt.Println("Can not write")
 	}
 }
