3f43e5b56c88a2483a30fd1542ff3eba4e794737
vinhhh <vinh.hh@holistics.io>
2026-05-15 22:36:59 +0700

    feat: multiple PING

--- app/server.go
+++ app/server.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"net"
 	"os"
+	// "time"
+	// "bytes"
 )
 
 func stop_server(l io.Closer) {
@@ -28,18 +30,31 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-
+	
 	defer stop_server(c)
 
+	
 	buf := make([]byte, 128)
-	_, err = c.Read(buf)
-	if err != nil {
-		fmt.Println("Can not read")
-	}
+	for {
+		_, err = c.Read(buf)
+		if err != nil {
+			break
+		}
+	
+		log.Printf("Read: %s", buf)
+		_, err = c.Write([]byte("+PONG\r\n"))
+		// if bytes.Equal(buf, []byte("*1\r\n$4\r\nPING\r\n")) {
+		// 	if err != nil {
+		// 		fmt.Println("Can not write")
+		// 	}
+		// } else {
+		// 	_,err = c.Write([]byte("+NO\r\n"))
+		// 	if err != nil {
+		// 		fmt.Println("Can not write")
+		// 	}
+		// }
 
-	log.Printf("Read: %s", buf)
-	_, err = c.Write([]byte("+PONG\r\n"))
-	if err != nil {
-		fmt.Println("Can not write")
-	}
+		// time.Sleep(time.Minute)
+
+		}
 }
